b14d0f03d07b2e9ba43af8d9de324e234e7bcddc
tetsuo shima <primaveraentalca@gmail.com>
2019-12-13 11:46:18 -0300

    agregar static query

--- src/components/radio.js
+++ src/components/radio.js
@@ -1,42 +1,48 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 import Checkbox from "rc-checkbox"
 import "rc-checkbox/assets/index.css"
 
 const Radio = ({ title, modo, onChange }) => (
-  <>
-    <h3>{title}</h3>
-    <div>
-      <p>
-        <label>
-          <Checkbox
-            defaultCheckedchecked
-            onChange={onChange}
-            value={1}
-            checked={modo === 1}
-          />
-          &nbsp; Modo 1
-        </label>
-      </p>
-    </div>
-
-    <div>
-      <p>
-        <label>
-          <Checkbox onChange={onChange} value={2} checked={modo === 2} />
-          &nbsp; Modo 2
-        </label>
-      </p>
-    </div>
-
-    <div>
-      <p>
-        <label>
-          <Checkbox onChange={onChange} value={3} checked={modo === 3} />
-          &nbsp; Modo 3
-        </label>
-      </p>
-    </div>
-  </>
+  <StaticQuery
+    query={graphql`
+      query AllMarkdownTitles {
+        allMarkdownRemark(
+          filter: { id: { nin: "c8690b52-3b0f-5c28-8283-746a5b957db6" } }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <h3>{title}</h3>
+        <div>
+          {data.allMarkdownRemark.edges.map(({ node }, i) => {
+            return (
+              <p key={i}>
+                <label>
+                  <Checkbox
+                    defaultCheckedchecked
+                    onChange={onChange}
+                    value={i + 1}
+                    checked={modo === i + 1}
+                  />
+                  &nbsp; {node.frontmatter.title}
+                </label>
+              </p>
+            )
+          })}
+        </div>
+      </>
+    )}
+  />
 )
 
 export default Radio
