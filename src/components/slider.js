ba72b78572b8169503996648c553205a99116cfd
tetsuo shima <primaveraentalca@gmail.com>
2019-12-15 02:22:50 -0300

    agregar descripcion

--- src/components/slider.js
+++ src/components/slider.js
@@ -3,11 +3,11 @@ import Slider from "rc-slider"
 
 import "rc-slider/assets/index.css"
 
-export default ({ title, marks, onChange }) => {
+export default ({ title, description, marks, onChange }) => {
   return (
     <div style={{ margin: "50px auto" }}>
       <h3>{title}</h3>
-
+      <p>{description}</p>
       <Slider
         min={1}
         max={Object.keys(marks).length}
