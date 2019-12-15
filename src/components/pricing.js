7b2c93593b75f38761516e18e35ddb03bdf34d31
marcelavaldes <marcelavaldesrojas@gmail.com>
2019-12-15 01:53:54 -0300

    desc4

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -45,7 +45,7 @@ const Pricing = () => {
       <Radio title={"Modalidades"} onChange={handleModo} modo={modo} />
       <Slider
         title={"Personas"}
-        description={"<b>Ahorra dinero</b> aprendiendo junto a familiares, amistades o con quien tu prefieras."}
+        description={"Ahorra dinero aprendiendo junto a familiares, amistades o con quien tu prefieras."}
         marks={{
           1: { label: "1 persona", style: {} },
           2: { label: "2 personas", style: {} },
@@ -56,7 +56,7 @@ const Pricing = () => {
       />
       <Slider
         title={"Horas"}
-        description={"Mientras más horas adquieras, <b>obtienes descuentos</b>."}
+        description={"Mientras más horas adquieras, obtienes descuentos."}
         marks={{
           1: { label: "1 Hora", style: {} },
           2: { label: "5 Horas", style: {} },
