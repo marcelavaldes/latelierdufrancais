fe23b1cf733548057480dabb735f4c9ded7b40d5
tetsuo shima <primaveraentalca@gmail.com>
2019-12-15 20:54:38 -0300

    limpiar layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Fragment } from "react"
 
-import lectora from "../../content/assets/lectora.svg"
-// import "./layout.css"
+import Header from "./header"
+import Main from "./main"
+import Footer from "./footer"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
-
-  if (location.pathname === rootPath) {
-    header = (
-      <h1>
-        <Link to={`/`}>{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3>
-        <Link to={`/`}>{title}</Link>
-      </h3>
-    )
-  }
+export default ({ location, title, children }) => {
   return (
-    <div>
-      <div>
-        <header>{header}</header>
-        <main>{children}</main>
-      </div>
-      <footer>
-        <div>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </div>
-        <img
-          src={lectora}
-          alt={""}
-          width={240}
-          style={{
-            transform: "scaleX(-1)",
-          }}
-        />
-      </footer>
-    </div>
+    <Fragment>
+      <Header location={location} title={title} />
+      <Main>{children}</Main>
+      <Footer />
+    </Fragment>
   )
 }
-
-export default Layout
